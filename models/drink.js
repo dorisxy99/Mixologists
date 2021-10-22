af7a233d895a2d99681c36cf3146a6d3110b269c
Doris Li <dorislxy@gmail.com>
2021-10-22 08:47:13 -0700

    finalized

--- models/drink.js
+++ models/drink.js
@@ -6,7 +6,7 @@ const reviewSchema = new Schema({
     content: {type: String, required: true},
     rating: {type: Number, min: 1, max: 5, default: 5},
     // Add the 3 new properties below
-    user: {Schema.Types.ObjectId, ref: 'User'},
+    user: {type: Schema.Types.ObjectId, ref: 'User'},
     userName: String,
     userAvatar: String,
   }, {
